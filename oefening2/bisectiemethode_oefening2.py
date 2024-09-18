ea68b5a9144bda4a3c205d2e08081124f77e15e9
Luka Verbrugghe <135641479+LukaVerbrugghe@users.noreply.github.com>
2024-09-18 11:06:53 +0200

    Oefening 2

--- oefening2/bisectiemethode_oefening2.py
+++ oefening2/bisectiemethode_oefening2.py
@@ -1,36 +1,30 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# zoek het kleinste snijpunt van de functies
-#     f(x) = 2^x en g(x) = x^2
-# door het interval herhaaldelijk te halveren
-
-# f(x) = 2^x
-# f = blauwe lijn
 def f(x):
     return x ** 3 - 8 * x - 3
 
-
-# g(x) = x^2
-# g = oranje
 def g(x):
     return x * 0
 
-
 # plot graphics
-x = np.linspace(-5, 5, 50)
+x = np.linspace(-5, 5, 500)
 y_1 = f(x)
 y_2 = g(x)
 plt.plot(x, y_1)
 plt.plot(x, y_2)
 
 # zoek snijpunt
-links = -1.0  # g(links) > f(links)
-rechts = 0.0  # g(rechts) < f(rechts)
+links = -3.0
+rechts = -2.0
 
+aantalIteraties = 1
 # Loop maken
 while rechts - links > 1E-13:
+    print("START NU ITERATIE:")
+    print(aantalIteraties)
+    print()
+    aantalIteraties += 1
     # midden zoeken => gemiddelde zoeken van de 2 waarden
     midden = (links + rechts) / 2
 
@@ -54,6 +48,9 @@ while rechts - links > 1E-13:
     print(links)
     print("Rechts")
     print(rechts)
+    print()
+    print()
+    print()
 
 plt.scatter(links, f(links), color='r')
 plt.show()
