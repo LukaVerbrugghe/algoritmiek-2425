cc457b29c772f26e3aded7a808cf8bd937fff068
Daan.Hautekeete <Daan.Hautekeete@E50401>
2024-09-25 09:18:38 +0200

    Toevoegen van opdracht

---
+++ Newton-Raphson methode/Probleemstelling 1/Probleemstelling1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# bepalen van nulpunt + recursie + opstellen van een functie in Python
+
+def newton_raphson(a, b, c, d, x0, tol, i):
+    # aantal iteraties
+    i = i + 1
+
+    # veelterm functie
+    f = a * x0 ** 3 + b * x0 ** 2 + c * x0 + d
+
+    # afgeleide van veelterm functie
+    df = 3 * a * x0 ** 2 + b * x0 ** 2 + c
+
+
+    # if (Vul aan, welke conditie hier?):
+
+
+    # else (Vul aan, welke conditie hier?)
+
+
+newton_raphson(0, 1, 0, -2, 1, 1E-13, 0)
