50b94bb6d8b7d87a896961e3bf0c5b3f5269b177
luka.verbrugghe <lotrdetailsys@gmail.com>
2024-10-02 09:09:43 +0200

    NewtonRapsonZonderRecursie.py loop godverdomme nog steeds oneindig

--- Oefening3/NewtonRapsonZonderRecursie.py
+++ Oefening3/NewtonRapsonZonderRecursie.py
@@ -10,15 +10,18 @@ def newton_raphson(a, b, c, d, x0, tol, i):
     x_n = x0
     x_n1 = 10000  # moet verschillende zijn van x0, anders geen iteraties
 
-    # while
+    while abs(x_n1 - x_n) > tol:
+        if i != 0:
+            x_n = x_n1
+        i = i + 1  # aantal iteraties
+        f = a * x_n ** 3 + b * x_n ** 2 + c * x_n + d  # veelterm functie
+        df = 3 * a * x_n ** 2 +2 * b * x_n + c
 
-    i = i + 1  # aantal iteraties
-    f = a * x_n ** 3 + b * x_n ** 2 + c * x_n + d  # veelterm functie
-    df = 3 * a * x_n ** 2 +2 * b * x_n + c
+        x_n1 = x_n - f / df
 
-    x_n1 = x_n - f / df
+        print(x_n1)
+        if abs(x_n1 - x_n) <= tol:
+            break
+    print(x_n)
 
-    print(x_n1)
-
-
-newton_raphson(0, 1, 0, 2, 1.5, 1E-13, 0)
+newton_raphson(0, 1, 0, 2, 1.5, 1E-2, 0)
