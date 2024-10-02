576b2264cc48fd4a414d58de0f4d80163856c65b
luka.verbrugghe <lotrdetailsys@gmail.com>
2024-10-02 08:56:14 +0200

    x_n1 berekend

--- Oefening3/NewtonRapsonZonderRecursie.py
+++ Oefening3/NewtonRapsonZonderRecursie.py
@@ -14,7 +14,11 @@ def newton_raphson(a, b, c, d, x0, tol, i):
 
     i = i + 1  # aantal iteraties
     f = a * x_n ** 3 + b * x_n ** 2 + c * x_n + d  # veelterm functie
-    df = 3 * a * x_n ** 2 + b * x_n ** 2 + c
+    df = 3 * a * x_n ** 2 +2 * b * x_n + c
+
+    x_n1 = x_n - f / df
+
+    print(x_n1)
 
 
 newton_raphson(0, 1, 0, 2, 1.5, 1E-13, 0)
