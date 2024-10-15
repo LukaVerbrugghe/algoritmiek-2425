721ec96d356d1336f992962e04e2591255ee0e60
Daan Hautekeete <hautekeete.daan@gmail.com>
2024-10-15 11:13:51 +0200

    Beginnen aan oefening Gauss-Jordan (wiskunde)

---
+++ Gauss-Jordan/Gauss-Jordan.py
@@ -0,0 +1,26 @@
+def verwissel(stelsel, index_1, index_2):  # vul aan
+    #index 1 = rij dat je wil wisselen
+    #index 2 = rij waarmee je de eerste ingegeven rij wisselt
+
+
+
+def deel_door(rij, deler):  # vul aan
+
+
+def trek_veelvoud_af(rij_1, rij_2, factor):  # vul aan
+
+
+def niet_nul_element(stelsel, k):  # vul aan
+
+
+def Gauss_jordan(stelsel):  # vul aan
+    print(stelsel)
+
+
+# test Gauss_jordan methode
+
+stelsel = [
+    [-3, 1, 0, 0],
+    [0, 1, -2, 2],
+    [-1, 0, 1, 7]
+]
