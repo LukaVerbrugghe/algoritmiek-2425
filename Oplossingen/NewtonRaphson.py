219c7341d00467d79115ddfd535a29ca0d2bfbe1
luka.verbrugghe <lotrdetailsys@gmail.com>
2024-10-16 09:11:32 +0200

    Foutopvang df = 0

--- Oplossingen/NewtonRaphson.py
+++ Oplossingen/NewtonRaphson.py
@@ -6,13 +6,15 @@ def newton_raphson(a, b, c, d, x0, tol, i):
     iteraties = []
     residual = []
     x_n = x0
-    x_n1 = -5  # moet verschillende zijn van x0, anders geen iteraties
+    x_n1 = 0  # moet verschillende zijn van x0, anders geen iteraties
 
     while abs(x_n1 - x_n) > tol:
         x_n = x_n1
         i = i + 1  # aantal iteraties
         f = a * x_n ** 3 + b * x_n ** 2 + c * x_n + d  # veelterm functie
         df = 3 * a * x_n ** 2 + 2 * b * x_n + c
+        if df == 0:
+            df = 1
         x_n1 = x_n - f / df
         iteraties.append(i)
         residual.append(abs(x_n1 - x_n))
@@ -42,4 +44,4 @@ def newton_raphson(a, b, c, d, x0, tol, i):
     plt.show()
 
 
-newton_raphson(1, 0, -8, -3, -4, 1E-13, 0)
+newton_raphson(0, 1, 0, -2, 2, 1E-13, 0)
