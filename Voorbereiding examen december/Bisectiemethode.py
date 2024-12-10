52cc516dc5e17248aadaf13c2ad649f23ca2e394
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2024-12-10 21:12:10 +0100

    Maken probleemstelling 2 bisectiemethode

--- Voorbereiding examen december/Bisectiemethode.py
+++ Voorbereiding examen december/Bisectiemethode.py
@@ -3,28 +3,27 @@ import numpy as np
 
 
 def f(x):
-    return 2.0 ** x
-
-def g(x):
-    return x ** 2
+    return x ** 3 - 8 * x - 3
 
 x = np.linspace(-3,5,50)
 y_1 = f(x)
-y_2 = g(x)
 plt.plot(x,y_1)
-plt.plot(x,y_2)
 
 # Snijpunt zoeken
-links = -1.0
-rechts = 0.0
+links = -4.0
+rechts = -2.0
+
+i = 0
 
 while rechts - links > 1E-13:
+    i+=1
     midden = (links + rechts) / 2
-    if g(midden) > f(midden):
+    if midden**3 - 8 * midden - 3 < 0:
         links = midden
     else:
         rechts = midden
 
+print(i)
 print(links)
 plt.scatter(links,f(links),color = 'r')
 plt.show()
