cb1dfc1692ba64bd07d7a39e5aebce869e820488
Luka Verbrugghe <135641479+LukaVerbrugghe@users.noreply.github.com>
2025-01-15 08:57:13 +0100

    commentaar

--- bijzondere rijen/alternerenderij.py
+++ bijzondere rijen/alternerenderij.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 
 def alternerendeRij(startwaarde):
+    # leeg array voor oplossingen (y-waarden)
     x = []
     # herhaal 10 keer
     for i in range(10):
+        # huidige oplossing aan het array toevoegen
         x.append(startwaarde)
+        # de nieuwe waarde berekenen
         startwaarde = -startwaarde * 2
+    # de oplossing weergeven
     print(x)
     for i in range(len(x)):
         plt.scatter(i, x[i])
--- opdrachten rijen/oefening rij.py
+++ opdrachten rijen/oefening rij.py
@@ -2,10 +2,14 @@ import matplotlib.pyplot as plt
 
 #rijen van un = (un-1 + 10) / 2
 def rij(startwaarde):
+    # een leeg array voor oplossing
     x = []
+    # we willen 10 rijen
     for i in range(10):
         x.append(startwaarde)
+        # functievoorschrift
         startwaarde = (startwaarde + 10) / 2
+    # de oplossing weergeven
     print(x)
     for i in range(len(x)):
         plt.scatter(i, x[i])
