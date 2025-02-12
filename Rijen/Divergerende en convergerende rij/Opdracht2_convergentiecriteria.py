b4b9360821750a25e05b7a8040e7798a2a96fef8
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-02-12 09:13:19 +0100

    toevoegen van oefeningen

---
+++ Rijen/Divergerende en convergerende rij/Opdracht2_convergentiecriteria.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+
+def berekening():
+    x = []
+    i = 0
+    j = 1
+    k =0
+
+    #beginwaarden in list steken
+    x.append(j)
+    x.append(k)
+
+    residuals = []
+    iteraties = []
+
+    #x[-1] = laatste element van lijst
+    #convergentiecriteria opstellen
+    while abs(x[-1] - x[-2]) > 0.00001:
+        i = i + 1
+        j = (-5*i) / (1 + 25*i)
+
+        #uitkomst toevoegen aan lijst
+        x.append(j)
+
+        #residual toevoegen aan lijst
+        residuals.append(abs(x[-1] - x[-2]))
+
+        #aantal iteraties toevoegen aan lijst
+        iteraties.append(i)
+
+        plt.scatter(i, j, color='r')
+
+    plt.show()
+
+berekening()
+
+#besluit: functie convergeerd naar een waarde namelijk -0,2 dus we gaan convergentiecriteria opstellen
