a81a1cb0228956669e7a992b6f2fc06966c6ce4f
Luka Verbrugghe <verbruggheluka0@gmail.com>
2025-02-19 09:00:58 +0100

    Gedrag op oneindig oefening a

---
+++ gedrag op oneindig/gedragOpOneindigA.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# gedrag op oneindig berekenen
+
+# functie voor het berekenen van de waarde van de functie met n als parameter
+x = []
+for i in range(100):
+    if i == 0:
+        j = 0
+        x.append(j)
+    else:
+        j = i ** 2 - (1/i)
+        x.append(j)
+    for i in range(len(x)):
+        plt.scatter(i, x[i], color='r')
+plt.show()
+
+# CONCLUSIE
+# De functie gaat steeds verder omhoog en convergeert niet naar een specifieke waarde.
