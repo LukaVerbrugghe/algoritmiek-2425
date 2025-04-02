32007011f9ede2694783c5f3aff1ab3d4287e9e0
Daan Hautekeete <Hautekeete.Daan@gmail.com>
2025-04-02 09:19:33 +0200

    WIP opdracht 2 punt-iteratieve methode

---
+++ Punt-iteratieve methode/Opdracht 2.py
@@ -0,0 +1,20 @@
+#initiele waarden instellen
+x_1 = 0
+x_2 = 0
+x_3 = 0
+
+#Aantal iteraties instellen
+i = 0
+
+# lists maken voor de oplossingen
+oplossingenx1 = []
+oplossingenx2 = []
+oplossingenx3 = []
+
+#initiele waarden toevoegen aan list
+oplossingenx1.append(x_1)
+oplossingenx2.append(x_2)
+oplossingenx3.append(x_3)
+
+x_1 = (5 - 2*x_2 - 4*x_3) / 2
+x_2 = (-16 + 4*x_1 + 8*x_3) / 3
